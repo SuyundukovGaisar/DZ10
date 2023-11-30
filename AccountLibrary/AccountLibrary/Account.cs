e2e224438450a52c129ab1637682e9baad87f1e7
SuyundukovGaisar <143888042+SuyundukovGaisar@users.noreply.github.com>
2023-11-30 22:25:00 +0300

    Add files via upload

---
+++ AccountLibrary/AccountLibrary/Account.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountLibrary
+{
+    public enum AccountType
+    {
+        Current,
+        Saving
+    }
+    public class Account
+    {
+        public int nextAccountNumber;
+        public int AccountNumber;
+        public decimal Balance;
+        public AccountType Accounttype;
+        private Queue<BankTransaction> Transactions = new Queue<BankTransaction>();
+
+        internal Account()
+        {
+            AccountNumber = GetNextAccountNumber();
+        }
+
+        public int GetNextAccountNumber()
+        {
+            return ++nextAccountNumber;
+        }
+
+
+        internal Account(decimal balance)
+        {
+            AccountNumber = GetNextAccountNumber();
+            Balance = balance;
+        }
+
+        internal Account(AccountType accounttype)
+        {
+            AccountNumber = GetNextAccountNumber();
+            Accounttype = accounttype;
+        }
+
+        internal Account(decimal balance, AccountType accounttype)
+        {
+            AccountNumber = GetNextAccountNumber();
+            Balance = balance;
+            Accounttype = accounttype;
+        }
+
+        public decimal TakeoffAccount(decimal amount)
+        {
+            BankTransaction transaction = new BankTransaction(DateTime.Now, -amount);
+            if (amount <= Balance && amount > 0)
+            {
+                Balance -= amount;
+                Transactions.Enqueue(transaction);
+                return Balance;
+
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public void PutonAccount(decimal amount)
+        {
+            BankTransaction transaction = new BankTransaction(DateTime.Now, amount);
+            if (amount < 0)
+            {
+                Console.WriteLine("Неправильно введено значение!");
+            }
+            else
+            {
+                Balance += amount;
+                Transactions.Enqueue(transaction);
+                Console.WriteLine($"Новый баланс: {Balance}");
+            }
+        }
+
+        public void PrintNewBalance()
+        {
+            Balance = 15000;
+            Console.WriteLine("Снять или пополнить?(после ввода нажмите enter)");
+            string str = Console.ReadLine();
+            str = str.ToLower();
+            if (str == "снять")
+            {
+                Console.WriteLine("Введите сумму для снятия(после ввода нажмите enter):");
+                decimal amount = decimal.Parse(Console.ReadLine());
+                decimal newBalance = TakeoffAccount(amount);
+                if (newBalance == -1)
+                {
+                    Console.WriteLine("Недостаточно средств на счете или неправильно введено значение для снятие!");
+                }
+                else
+                {
+                    Console.WriteLine($"Новый баланс: {newBalance}");
+                    // Получаем время транзакции с помощью метода Peek
+                    // Первый элемент в очереди транзакций
+                    DateTime transactionTime = Transactions.Peek().DateTime;
+                    // Получаем сумму транзакции с помощью метода Peek
+                    decimal transactionSum = Transactions.Peek().Sum;
+                    Console.WriteLine($"Время транзакции: {transactionTime}");
+                    Console.WriteLine($"Сумма транзакции: {transactionSum}");
+                    Dispose();
+
+                }
+            }
+            else if (str == "пополнить")
+            {
+                Console.WriteLine("Введите сумму для пополнения(после ввода нажмите enter):");
+                decimal amount = decimal.Parse(Console.ReadLine());
+                PutonAccount(amount);
+                DateTime transactionTime = Transactions.Peek().DateTime;
+                decimal transactionSum = Transactions.Peek().Sum;
+                Console.WriteLine($"Время транзакции: {transactionTime}");
+                Console.WriteLine($"Сумма транзакции: {transactionSum}");
+                Dispose();
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Номер: {AccountNumber}");
+            Console.WriteLine($"Баланс: {Balance}");
+            Console.WriteLine($"Тип счета: {Accounttype}");
+        }
+        public void TransferMoney(Account account1, decimal amount)
+        {
+            decimal newBalance = account1.TakeoffAccount(amount);
+
+            if (newBalance != -1)
+            {
+                account1.AccountNumber++;
+                Balance += amount;
+                Console.WriteLine($"Новый баланс на счету {AccountNumber}: {Balance}");
+                Console.WriteLine($"Новый баланс на счету {account1.AccountNumber}: {newBalance}");
+                DateTime transactionTime = account1.Transactions.Peek().DateTime;
+                decimal transactionSum = account1.Transactions.Peek().Sum;
+                Console.WriteLine($"Время транзакции: {transactionTime}");
+                Console.WriteLine($"Сумма транзакции: {transactionSum}");
+                account1.Dispose();
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно средств на счете или неправильно введено значение для перевода!");
+            }
+        }
+        public void Dispose()
+        {
+            if (Transactions.Count > 0)
+            {
+                using (StreamWriter writer = new StreamWriter("Transactions.txt", true))
+                {
+                    foreach (BankTransaction transaction in Transactions)
+                    {
+                        writer.WriteLine($"Дата: {transaction.DateTime}, Сумма: {transaction.Sum}");
+                    }
+                }
+            }
+            GC.SuppressFinalize(this);
+        }
+        public override bool Equals(object obj)
+        {
+            Account account = obj as Account;
+            if (account == null)
+            {
+                return false;
+            }
+            return AccountNumber == account.AccountNumber && Balance == account.Balance && Accounttype == account.Accounttype;
+        }
+        public override int GetHashCode()
+        {
+            return AccountNumber.GetHashCode() ^ Balance.GetHashCode() ^ Accounttype.GetHashCode();
+        }
+        public static bool operator ==(Account account1, Account account2)
+        {
+            if (object.ReferenceEquals(account1, account2))
+            {
+                return true;
+            }
+
+            if (object.ReferenceEquals(account1, null) || object.ReferenceEquals(account2, null))
+            {
+                return false;
+            }
+
+            return account1.Equals(account2);
+        }
+        public static bool operator !=(Account account1, Account account2)
+        {
+            return !(account1 == account2);
+        }
+        public override string ToString()
+        {
+            return $"Номер: {AccountNumber}, Баланс: {Balance}, Тип счета: {Accounttype}";
+        }
+    }
+    public class BankTransaction
+    {
+        public readonly DateTime DateTime;
+        public readonly decimal Sum;
+        internal BankTransaction(DateTime dateTime, decimal sum)
+        {
+            DateTime = dateTime;
+            Sum = sum;
+        }
+    }
+    internal class AccountFactory
+    {
+        Hashtable accounts = new Hashtable();
+        public int CreateAccount()
+        {
+            Account account = new Account();
+            accounts.Add(account.AccountNumber, account);
+            return account.AccountNumber;
+        }
+        public int CreateAccount(decimal balance)
+        {
+            Account account = new Account(balance);
+            accounts.Add(account.AccountNumber, account);
+            return account.AccountNumber;
+        }
+
+        public int CreateAccount(AccountType accounttype)
+        {
+            Account account = new Account(accounttype);
+            accounts.Add(account.AccountNumber, account);
+            return account.AccountNumber;
+        }
+
+        public int CreateAccount(decimal balance, AccountType accounttype)
+        {
+            Account account = new Account(balance, accounttype);
+            accounts.Add(account.AccountNumber, account);
+            return account.AccountNumber;
+        }
+        public void CloseAccount(int accountNumber)
+        {
+            if (accounts.ContainsKey(accountNumber))
+            {
+                accounts.Remove(accountNumber);
+            }
+        }
+    }
+}
---
+++ BuildingLibrary/BuildingLibrary/Building.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingLibrary
+{
+    public class Building
+    {
+        public static int nextBuildingNumber;
+        public int BuildingNumber;
+        public int Height;
+        public int Floors;
+        public int Floats;
+        public int entrances;
+        public int HeightFloor;
+        public int FloatsEntrance;
+        public int FloatsFloor;
+
+        internal Building()
+        {
+            BuildingNumber = NextBuildingNumber();
+        }
+        internal Building(int height, int floors)
+        {
+            BuildingNumber = NextBuildingNumber();
+            Height = height;
+            Floors = floors;
+        }
+        internal Building(int height, int floors, int floats, int entrances)
+        {
+            BuildingNumber = NextBuildingNumber();
+            Height = height;
+            Floors = floors;
+            Floats = floats;
+            this.entrances = entrances;
+        }
+        internal Building(int height, int floors, int floats, int entrances, int heightFloor, int floatsEntrance, int floatsFloor)
+        {
+            BuildingNumber = NextBuildingNumber();
+            Height = height;
+            Floors = floors;
+            Floats = floats;
+            this.entrances = entrances;
+            HeightFloor = heightFloor;
+            FloatsEntrance = floatsEntrance;
+            FloatsFloor = floatsFloor;
+        }
+
+        public int GenerateBuildingNumber()
+        {
+            Random random = new Random();
+            nextBuildingNumber = random.Next();
+            return nextBuildingNumber;
+        }
+
+        public static int NextBuildingNumber()
+        {
+            return ++nextBuildingNumber;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Номер здания: {BuildingNumber}");
+            Console.WriteLine($"Высота: {Height}");
+            Console.WriteLine($"Кол-во этажей: {Floors}");
+            Console.WriteLine($"Кол-во квартир: {Floats}");
+            Console.WriteLine($"Кол-во подъездов: {entrances}");
+            Console.WriteLine($"Следующий номер здания: {nextBuildingNumber = NextBuildingNumber()}");
+        }
+        public void CalculateHeightFloor()
+        {
+            HeightFloor = Height / Floors;
+        }
+
+        public void CalculateFloatsEntrance()
+        {
+            FloatsEntrance = Floats / entrances;
+        }
+
+        public void CalculateFloatsFloor()
+        {
+            FloatsFloor = Floats / Floors;
+        }
+        public void PrintCalculaions()
+        {
+            Console.WriteLine($"Высота этажа: {HeightFloor}");
+            Console.WriteLine($"Кол-во квартир в подъезде: {FloatsEntrance}");
+            Console.WriteLine($"Кол-во квартир на этаже: {FloatsFloor}");
+        }
+    }
+    public class Creator
+    {
+        static Hashtable buildings = new Hashtable();
+        public static Building CreateBuild()
+        {
+            Building building = new Building();
+            buildings.Add(building.BuildingNumber, building);
+            return building;
+        }
+        public static Building CreateBuild(int height, int floors)
+        {
+            Building building = new Building(height, floors);
+            buildings.Add(building.BuildingNumber, building);
+            return building;
+        }
+
+        public static Building CreateBuild(int height, int floors, int floats, int entrances)
+        {
+            Building building = new Building(height, floors, floats, entrances);
+            buildings.Add(building.BuildingNumber, building);
+            return building;
+        }
+
+        public static Building CreateBuild(int height, int floors, int floats, int entrances, int heightFloor, int floatsEntrance, int floatsFloor)
+        {
+            Building building = new Building(height, floors, floats, entrances, heightFloor, floatsEntrance, floatsFloor);
+            buildings.Add(building.BuildingNumber, building);
+            return building;
+        }
+
+        public static void DeleteBuild(int buildingNumber)
+        {
+            if (buildings.ContainsKey(buildingNumber))
+            {
+                buildings.Remove(buildingNumber);
+            }
+        }
+
+    }
+}
